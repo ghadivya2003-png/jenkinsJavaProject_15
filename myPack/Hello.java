4c5d6dcb851c0b4357b216eb772686d208b0c368
ghadivya2003-png <ghadivya2003@gmail.com>
2026-05-04 22:15:47 +0530

    Add files via upload

---
+++ myPack/Hello.java
@@ -0,0 +1,12 @@
+package myPack;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        System.out.println("Hello from Jenkins CI!");
+
+	}
+
+}
